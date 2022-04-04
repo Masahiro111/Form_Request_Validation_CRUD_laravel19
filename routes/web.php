5dcc3e388ca58a2e1277f9781ddfc9b31739a7c2
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-04-04 17:56:22 +0900

    save

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('students', StudentController::class);
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -21,4 +24,4 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
